8e212214589f64005a4efb9ce14fe752cc15a7d6
Akhilan Theiventhiran <Akhilan@Akhilans-MacBook-Pro.local>
2026-04-21 09:44:36 +0530

    feat: implement analytics aggregation pipeline

---
+++ analyticsAggregation.js
@@ -0,0 +1,12 @@
+// analyticsAggregation.js
+// Implements aggregation logic for analytics data
+
+function aggregateAnalyticsData(data) {
+    // Example: Count total events by type
+    return data.reduce((acc, event) => {
+        acc[event.type] = (acc[event.type] || 0) + 1;
+        return acc;
+    }, {});
+}
+
+module.exports = aggregateAnalyticsData;
